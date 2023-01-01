7eca27deca70becd77b35d5220fd7803a48eec45
PrasanthKumar Yernagula <86708678+Prashanthkumarpk@users.noreply.github.com>
2023-01-01 22:56:07 +0530

    Create ConvertCelsiusToFahrenheit.cpp

---
+++ Mathematics/ConvertCelsiusToFahrenheit.cpp
@@ -0,0 +1,18 @@
+class Solution{
+    public:
+    
+    //You need to complete this function
+    double cToF(int C)
+    {
+        return 1.8*C +32;
+        //YoReur code here
+    }
+};
+
+Input:
+C = 32
+Output: 89
+Explanation: Using the conversion 
+formula of celsius to farhenheit , it
+can be calculated that, for 32 degree
+C, the temperature in Fahrenheit = 89.
