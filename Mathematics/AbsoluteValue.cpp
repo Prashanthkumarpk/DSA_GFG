8630aced4eb416950b48b823e2eb5edaf3999570
PrasanthKumar Yernagula <86708678+Prashanthkumarpk@users.noreply.github.com>
2023-01-01 22:54:47 +0530

    Create AbsoluteValue.cpp

---
+++ Mathematics/AbsoluteValue.cpp
@@ -0,0 +1,16 @@
+You are given an interger I, find the absolute value of the interger I.
+  
+class Solution{
+    public:
+        int absolute(int I) {
+ if (I < 0 )   return -1*I;
+ else  return I;
+     
+ 
+    }
+  
+  Input:
+I = -32
+Output: 32
+Explanation: 
+The absolute value of -32 is 32.
